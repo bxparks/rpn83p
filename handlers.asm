b74e656483540e28e57d3e03426c3e16760e41da
Brian Park <brian@xparks.net>
2023-07-12 21:56:45 -0700

    rpn83p: add liftStack()

--- handlers.asm
+++ handlers.asm
@@ -196,9 +196,33 @@ handleKeyChsSetNegative:
 ; Destroys: all
 handleKeyEnter:
     call parseNum
-    call debugOP1
-    ; call liftStack
+    ; call debugOP1
+    call liftStack
 
     ld hl, displayFlags
     set displayFlagsDirty, (hl)
+    ret
+
+; Function: Move the OP1 register to X, and lift the RPN stack.
+; Input: OP1
+; Output: T=Z; Z=Y; Y=X; X=OP1
+liftStack:
+    ; save OP1 in lastX
+    bcall(_StoR)
+
+    ; T = Z
+    bcall(_ThetaName)
+    bcall(_RclVarSym)
+    bcall(_StoT)
+    ; Z = Y
+    bcall(_RclY)
+    bcall(_StoTheta)
+    ; Y = X
+    bcall(_RclX)
+    bcall(_StoY)
+    ; X = lastX
+    bcall(_RName)
+    bcall(_RclVarSym)
+    bcall(_StoX)
+
     ret
