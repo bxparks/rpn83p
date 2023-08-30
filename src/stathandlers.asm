c6f9102c0fb9d91b3c037adf8c037bf0f5a5f67b
Brian Park <brian@xparks.net>
2023-08-30 15:11:06 -0700

    stathandlers: create arrays of register indexes for various curve fitting models

--- src/stathandlers.asm
+++ src/stathandlers.asm
@@ -11,7 +11,7 @@
 ;   - https://en.wikipedia.org/wiki/Covariance_and_correlation
 ;-----------------------------------------------------------------------------
 
-; Direct storage register indexes for various STAT components.
+; Direct register indexes for various STAT components.
 statRegX equ 11
 statRegX2 equ 12
 statRegY equ 13
@@ -25,6 +25,65 @@ statRegLnY2 equ 20
 statRegLnXLnY equ 21
 statRegXLnY equ 22
 statRegYLnX equ 23
+
+; Indirect register indexes for various curve fitting algorithms.
+statIndX equ 0
+statIndX2 equ 1
+statIndY equ 2
+statIndY2 equ 3
+statIndXY equ 4
+statIndN equ 5
+
+; Storage registers for Linear curve fitting.
+;   y = b + m x
+statLinearRegs:
+    .db statRegX
+    .db statRegX2
+    .db statRegY
+    .db statRegY2
+    .db statRegXY
+    .db statRegN
+
+; Storage registers for Exponential curve fitting.
+;   y = b e^(m x)
+;   ln y = ln b + m x
+;   y' = b' + m x, where
+;       y' = ln y
+;       b' = ln b
+statExpRegs:
+    .db statRegX
+    .db statRegX2
+    .db statRegLnY
+    .db statRegLnY2
+    .db statRegXLnY
+    .db statRegN
+
+; Storage registers for Logarithmic curve fitting.
+;   y = b + m ln x
+;   y = b + m x', where
+;       x' = ln x
+statLogRegs:
+    .db statRegLnX
+    .db statRegLnX2
+    .db statRegY
+    .db statRegY2
+    .db statRegYLnX
+    .db statRegN
+
+; Storage registers for Power curve fitting.
+;   y = b x^m
+;   ln y = ln b + m ln x
+;   y' = b' + m x', where
+;       y' = ln y
+;       b' = ln b
+;       x' = ln x
+statPowerReg:
+    .db statRegLnX
+    .db statRegLnX2
+    .db statRegLnY
+    .db statRegLnY2
+    .db statRegLnXLnY
+    .db statRegN
 
 ;-----------------------------------------------------------------------------
 ; STAT Menu handlers.
@@ -101,6 +160,7 @@ mStatSumHandler:
 ; Description: Calculate the average of X and Y into X and Y registers.
 mStatMeanHandler:
     call closeInputBuf
+    ld ix, statLinearRegs
     call statMean
     jp pushXY
 
@@ -110,6 +170,7 @@ mStatMeanHandler:
 ;   X: Mean of X weighted by Y = Sum(X,Y) / Sum(Y)
 mStatWeightedMeanHandler:
     call closeInputBuf
+    ld ix, statLinearRegs
     call statWeightedMean ; OP1=WeightedY, OP2=WeightedX
     jp pushXY
 
@@ -129,6 +190,7 @@ mStatNHandler:
 ; Destroys: A, OP2, OP3, OP4
 mStatPopSdevHandler:
     call closeInputBuf
+    ld ix, statLinearRegs
     call statStdDev
     jp pushXY
 
@@ -139,6 +201,7 @@ mStatPopSdevHandler:
 ; Destroys: A, OP2, OP3, OP4
 mStatSampleSdevHandler:
     call closeInputBuf
+    ld ix, statLinearRegs
     call statVariance ; OP1=VAR(Y), OP2=VAR(X)
     ; Multiply each VAR(x) with N/(N-1)
     bcall(_PushRealO2) ; FPS=VAR(X)
@@ -160,6 +223,7 @@ mStatSampleSdevHandler:
 ; Destroys: A, OP2, OP3, OP4
 mStatPopCovHandler:
     call closeInputBuf
+    ld ix, statLinearRegs
     call statCovariance
     jp pushX
 
@@ -170,6 +234,7 @@ mStatPopCovHandler:
 ; Destroys: A, OP2, OP3, OP4
 mStatSampleCovHandler:
     call closeInputBuf
+    ld ix, statLinearRegs
     call statCovariance ; OP1=PCOV(X,Y)
     call statFactorPopToSampleOP2 ; OP2=N/(N-1)
     bcall(_FPMult); OP1=SCOV(X,Y)
@@ -182,7 +247,8 @@ mStatForcastYHandler:
     call closeInputBuf
     call rclX ; OP1=X
     bcall(_PushRealO1) ; FPS=X
-    call statLinearFit ; OP1=intercept,OP2=slope
+    ld ix, statLinearRegs
+    call statLeastSquareFit ; OP1=intercept,OP2=slope
     call exchangeFPSOP1 ; OP1=X, FPS=intercept
     bcall(_FPMult) ; OP1=slope*X
     bcall(_PopRealO2) ; OP2=intercept
@@ -194,7 +260,8 @@ mStatForcastXHandler:
     call closeInputBuf
     call rclX ; OP1=X=y
     bcall(_PushRealO1) ; FPS=y
-    call statLinearFit ; OP1=intercept,OP2=slope
+    ld ix, statLinearRegs
+    call statLeastSquareFit ; OP1=intercept,OP2=slope
     call exchangeFPSOP2 ; OP2=y, FPS=slope
     bcall(_InvSub) ; OP1=y-intercept
     bcall(_PopRealO2) ; OP2=slope
@@ -204,19 +271,22 @@ mStatForcastXHandler:
 ; Description: Calculate the least square fit slope into X register.
 mStatSlopeHandler:
     call closeInputBuf
-    call statLinearFit ; OP1=intercept,OP2=slope
+    ld ix, statLinearRegs
+    call statLeastSquareFit ; OP1=intercept,OP2=slope
     bcall(_OP1ExOP2)
     jp pushX
 
 ; Description: Calculate the least square fit intercept into X register.
 mStatInterceptHandler:
     call closeInputBuf
-    call statLinearFit ; OP1=intercept,OP2=slope
+    ld ix, statLinearRegs
+    call statLeastSquareFit ; OP1=intercept,OP2=slope
     jp pushX
 
 ; Description: Calculate the correlation coefficient into X register.
 mStatCorrelationHandler:
     call closeInputBuf
+    ld ix, statLinearRegs
     call statCorrelation
     jp pushX
 
@@ -451,40 +521,42 @@ statSigmaMinusLogYZero:
 ;-----------------------------------------------------------------------------
 
 ; Description: Calculate the average of X and Y into OP1 and OP2 registers.
+; Input:
+;   IX=pointer to list of stat registers (e.g. statLinearRegs, statExpRegs)
 ; Output:
 ;   OP1=<Y>
 ;   OP2=<X>
 statMean:
-    ld a, statRegX
+    ld a, (ix + statIndX)
     call rclNN
-    ld a, statRegN
+    ld a, (ix + statIndN)
     call rclNNToOP2
-    bcall(_FPDiv) ; OP1=Xmean
-    bcall(_PushRealO1)
+    bcall(_FPDiv) ; OP1=<X>
+    bcall(_PushRealO1) ; FPST=<X>
     ;
-    ld a, statRegY
+    ld a, (ix + statIndY)
     call rclNN
-    ld a, statRegN
-    call rclNNToOP2
     bcall(_FPDiv) ; OP1=<Y>
     bcall(_PopRealO2) ; OP2=<X>
     ret
 
 ; Description: Calculate the weighted mean of Y and X into OP1 and OP2,
 ; respectively.
+; Input:
+;   IX=pointer to list of stat registers (e.g. statLinearRegs, statExpRegs)
 ; Output:
 ;   OP1: Mean of Y weighted by X = Sum(X,Y) / Sum(X)
 ;   OP2: Mean of X weighted by Y = Sum(X,Y) / Sum(Y)
 statWeightedMean:
-    ld a, statRegXY
+    ld a, (ix + statIndXY)
     call rclNN
     bcall(_PushRealO1) ; FPS=SumXY
-    ld a, statRegY
+    ld a, (ix + statIndY)
     call rclNNToOP2
     bcall(_FPDiv) ; OP1=SumXY/SumY
     ;
     call exchangeFPSOP1 ; OP1=SumXY, FPS=SumXY/SumY
-    ld a, statRegX
+    ld a, (ix + statIndX)
     call rclNNToOP2 ; OP2=SumX
     bcall(_FPDiv) ; OP1=SumXY/SumX=WeightedY
     bcall(_PopRealO2); OP2=SumXY/SumY=WeightedX
@@ -509,6 +581,8 @@ statFactorPopToSampleOP2:
     ret
 
 ; Description: Calculate the population standard deviation.
+; Input:
+;   IX=pointer to list of stat registers (e.g. statLinearRegs, statExpRegs)
 ; Output:
 ;   OP1=PDEV<Y>
 ;   OP2=PDEV<X>
@@ -525,6 +599,8 @@ statStdDevAltEntry:
 
 ; Description: Calculate the population variance.
 ; Var(X) = Sum(X_i^2 - <X>^2) / N = <X^2> - <X>^2
+; Input:
+;   IX=pointer to list of stat registers (e.g. statLinearRegs, statExpRegs)
 ; Output:
 ;   OP1: VAR<Y>
 ;   OP2: VAR<X>
@@ -532,34 +608,34 @@ statStdDevAltEntry:
 ; TODO: The algorithms for VAR<X> and VAR<Y> are identical. We should be able
 ; to extract that into a common routine to save memory.
 statVariance:
-    ld a, statRegX
+    ld a, (ix + statIndX)
     call rclNN
-    ld a, statRegN
+    ld a, (ix + statIndN)
     call rclNNToOP2
     bcall(_FPDiv)
     bcall(_FPSquare) ; OP1=<X>^2
     bcall(_PushRealO1)
     ;
-    ld a, statRegX2
+    ld a, (ix + statIndX2)
     call rclNN
-    ld a, statRegN
+    ld a, (ix + statIndN)
     call rclNNToOP2
     bcall(_FPDiv) ; OP1=<X^2>
     bcall(_PopRealO2) ; OP2=<X>^2
     bcall(_FPSub)
     bcall(_PushRealO1) ; OP1=VAR<X>
     ;
-    ld a, statRegY
+    ld a, (ix + statIndY)
     call rclNN
-    ld a, statRegN
+    ld a, (ix + statIndN)
     call rclNNToOP2
     bcall(_FPDiv)
     bcall(_FPSquare) ; OP1=<Y>^2
     bcall(_PushRealO1)
     ;
-    ld a, statRegY2
+    ld a, (ix + statIndY2)
     call rclNN
-    ld a, statRegN
+    ld a, (ix + statIndN)
     call rclNNToOP2
     bcall(_FPDiv) ; OP1=<Y^2>
     bcall(_PopRealO2) ; OP2=<Y>^2
@@ -569,29 +645,31 @@ statVariance:
     ret
 
 ; Description: Calculate the population covariance of X and Y.
-; PCOV(X, Y) = <XY> - <X><Y>).
+; PCOV(X, Y) = <XY> - <X><Y>
+; Input:
+;   IX=pointer to list of stat registers (e.g. statLinearRegs, statExpRegs)
 ; See https://en.wikipedia.org/wiki/Covariance_and_correlation
 ; Output:
-;   - OP1: PCOV<X,Y>
+;   OP1: PCOV<X,Y>
 ; Destroys: A, OP2, OP3, OP4
 statCovariance:
     ; Extract N
-    ld a, statRegN
+    ld a, (ix + statIndN)
     call rclNNToOP2
     bcall(_OP2ToOP4) ; OP4=N
     ; Calculate <XY>
-    ld a, statRegXY
+    ld a, (ix + statIndXY)
     call rclNN
     bcall(_FPDiv) ; OP1=<XY>, uses OP3
     bcall(_PushRealO1) ; FPS=<XY>
     ; Calculate <X>
-    ld a, statRegX
+    ld a, (ix + statIndX)
     call rclNN
     bcall(_OP4ToOP2) ; OP2=N
     bcall(_FPDiv) ; OP1=<X>
     bcall(_PushRealO1) ; FPS=<X>
     ; Calculate <Y>
-    ld a, statRegY
+    ld a, (ix + statIndY)
     call rclNN
     bcall(_OP4ToOP2) ; OP2=N
     bcall(_FPDiv) ; OP1=<Y>
@@ -605,6 +683,10 @@ statCovariance:
 
 ; Description: Calculate the correslation coeficient into OP1.
 ; R(X,,Y) = COV(X,Y)/StdDev(X)/StdDev(Y).
+; Input:
+;   IX=pointer to list of stat registers (e.g. statLinearRegs, statExpRegs)
+; Output:
+;   OP1=correlation coefficient in the range of [-1, 1].
 ;
 ; Either Population or Sample versions can be used, because the N/(N-1) terms
 ; cancel out. See https://en.wikipedia.org/wiki/Correlation
@@ -620,14 +702,16 @@ statCorrelation:
 
 ;-----------------------------------------------------------------------------
 
-; Description: Calculate the least square linear fit.
+; Description: Calculate the least square fit.
+; Input:
+;   IX=pointer to list of stat registers (e.g. statLinearRegs, statExpRegs)
 ; Output:
 ;   OP1=SLOP(X,Y) = CORR(X,Y) (StdDev(Y)/StdDev(X)).
 ;   OP2=YINT(X,,Y) = <Y> - SLOP(X,Y) * <X>
 ;
 ; Either Population or Sample can be used, because the N/(N-1) terms cancel
 ; out. See https://en.wikipedia.org/wiki/Simple_linear_regression
-statLinearFit:
+statLeastSquareFit:
     ; Calculate slope.
     call statStdDev ; OP1=PDEV(Y), OP2=PDEV(X)
     bcall(_FPDiv) ; OP1=PDEV(Y)/PDEV(X)
