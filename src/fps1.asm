6dca2dc4a73d68ddd4b4b7a8567355edeea3e0c1
Brian Park <brian@xparks.net>
2024-02-07 13:37:20 -0800

    fps1.asm: add pushRaw9Op1(),popRaw9Op1()

--- src/fps1.asm
+++ src/fps1.asm
@@ -46,9 +46,10 @@ PopRpnObject5:
 ; RpnObject type (e.g. real, complex, date, etc).
 ; Input:
 ;   - HL:pointer to an OP register
+;   - (FPS): pointer to end of FPS stack
 ; Output:
-;   - FPS increased by 18 bytes
 ;   - HL=pointer to RpnObject on FPS
+;   - (FPS) increased by 18 bytes
 ; Destroys: all
 pushRpnObject:
     push hl ; stack=[OPx]
@@ -58,7 +59,7 @@ pushRpnObject:
     ld hl, 18
     bcall(_AllocFPS1)
     ; copy OP1 into RpnObject on FPS
-    pop de ; stack=[Opx]; DE=FPS1
+    pop de ; stack=[OPx]; DE=FPS1
     pop hl ; stack=[]; HL=OPx
     push de ; stack=[FPS1]
     ld bc, 9
@@ -71,29 +72,97 @@ pushRpnObject:
     pop hl ; stack=[]; HL=FPS1
     ret
 
-; Description: Pop the RpnObject from FPS to OP1/OP2 no matter the RpnObject
-; type.
+; Description: Pop the RpnObject from FPS to DE no matter the RpnObject type.
 ; Input:
-;   - DE: pointer to an OP register
-;   - FPS
+;   - DE: pointer to an OPx register
+;   - (FPS): pointer to end of stack
 ; Output:
-;   - OPx register updated
-;   - FPS decreased by 18 bytes
+;   - (OPx) register filled
+;   - (FPS) decreased by 18 bytes
 ; Destroys: all
 popRpnObject:
     ld hl, (FPS)
+    ; copy RpnObject on FPS to OPx pointed by DE
     ld bc, 18
     or a ; CF=0
     sbc hl, bc ; HL=pointer to RpnObject on FPS
-    ; copy RpnObject on FPS to OP1
     ld bc, 9
     ldir
-    inc de ; skip 2 extra bytes in OP1
+    ; copy RpnObject on FPS to OP{x+1}
+    inc de ; skip 2 extra bytes in OPx
     inc de
-    ; copy RpnObject on FPS to OP2
     ld bc, 9
     ldir
     ; deallocate
     ld de, 18
+    bcall(_DeallocFPS1)
+    ret
+
+;-----------------------------------------------------------------------------
+
+; Description: Push 9 raw bytes from OP1 into FPS.
+; Output: HL=pointer to raw 9 bytes on FPS.
+; Destroys: all
+pushRaw9Op1:
+    ld hl, OP1
+    jr pushRaw9
+
+popRaw9Op1:
+    ld de, OP1
+    jr popRaw9
+
+; Description: Push 9 raw bytes from OP1 into FPS.
+; Output: HL=pointer to raw 9 bytes on FPS.
+; Destroys: all
+pushRaw9Op2:
+    ld hl, OP2
+    jr pushRaw9
+
+popRaw9Op2:
+    ld de, OP2
+    jr popRaw9
+
+;-----------------------------------------------------------------------------
+
+; Description: Push 9 raw bytes from OPx to FPS.
+; Input:
+;   - HL:pointer to an OP register
+; Output:
+;   - (FPS) increased by 9 bytes
+;   - HL=pointer to Raw9 on FPS
+; Destroys: all
+pushRaw9:
+    push hl ; stack=[OPx]
+    ld hl, (FPS) ; HL=FPS0=futureFPS-9
+    push hl ; stack=[OPx,FPST]
+    ; allocate space on stack
+    ld hl, 9
+    bcall(_AllocFPS1)
+    ; copy 9 raw bytes in OP1 to FPS
+    pop de ; stack=[OPx]; DE=FPS0
+    pop hl ; stack=[]; HL=OPx
+    push de ; stack=[FPS0]
+    ld bc, 9
+    ldir
+    pop hl ; stack=[]; HL=FPS0
+    ret
+
+; Description: Pop the Raw 9 bytes from FPS to DE.
+; Input:
+;   - DE: pointer to an OPx register
+;   - (FPS): pointer to end of FPS stack
+; Output:
+;   - (OPx) register filled
+;   - (FPS) decreased by 9 bytes
+; Destroys: all
+popRaw9:
+    ld hl, (FPS)
+    ; copy raw 9 bytes on FPS to DE
+    ld bc, 9
+    or a ; CF=0
+    sbc hl, bc ; HL=pointer to raw 9 bytes on FPS
+    ldir
+    ; deallocate
+    ld de, 9
     bcall(_DeallocFPS1)
     ret
