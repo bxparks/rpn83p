fcf9b281324e6fc04e423d71b1e49ec386e94d15
Brian Park <brian@xparks.net>
2024-02-16 12:33:32 -0800

    format.asm: move reverseString() from formatinteger32.asm, to clean up dependencies

--- src/format.asm
+++ src/format.asm
@@ -19,3 +19,37 @@ convertAToChar:
 convertAToCharDecimal:
     add a, '0'
     ret
+
+;-----------------------------------------------------------------------------
+
+; Description: Reverses the chars of the string referenced by HL.
+; Input:
+;   - HL: reference to C-string
+;   - B: number of characters
+; Output: string in (HL) reversed
+; Destroys: A, B, DE, HL
+reverseString:
+    ; test for 0-length string
+    ld a, b
+    or a
+    ret z
+
+    ld e, b
+    ld d, 0
+    ex de, hl
+    add hl, de
+    ex de, hl ; DE = DE + B = end of string
+    dec de
+
+    srl b ; B = num / 2
+    ret z ; NOTE: Failing to check for this zero took 2 days to debug!
+reverseStringLoop:
+    ld a, (de)
+    ld c, (hl)
+    ld (hl), a
+    ld a, c
+    ld (de), a
+    inc hl
+    dec de
+    djnz reverseStringLoop
+    ret
--- src/formatinteger32.asm
+++ src/formatinteger32.asm
@@ -235,37 +235,3 @@ truncateTrailingZerosEnd:
     pop de
     pop hl
     ret
-
-;-----------------------------------------------------------------------------
-
-; Description: Reverses the chars of the string referenced by HL.
-; Input:
-;   - HL: reference to C-string
-;   - B: number of characters
-; Output: string in (HL) reversed
-; Destroys: A, B, DE, HL
-reverseString:
-    ; test for 0-length string
-    ld a, b
-    or a
-    ret z
-
-    ld e, b
-    ld d, 0
-    ex de, hl
-    add hl, de
-    ex de, hl ; DE = DE + B = end of string
-    dec de
-
-    srl b ; B = num / 2
-    ret z ; NOTE: Failing to check for this zero took 2 days to debug!
-reverseStringLoop:
-    ld a, (de)
-    ld c, (hl)
-    ld (hl), a
-    ld a, c
-    ld (de), a
-    inc hl
-    dec de
-    djnz reverseStringLoop
-    ret
