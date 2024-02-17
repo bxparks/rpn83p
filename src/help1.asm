51085ffbf0fc18a1930d4182547d7a39a42f4e9f
Brian Park <brian@xparks.net>
2024-02-17 09:40:53 -0800

    help1.asm: add 2024 to copyright notice

--- src/help1.asm
+++ src/help1.asm
@@ -131,7 +131,7 @@ helpPageCount equ (helpPagesEnd-helpPages)/2
 msgHelpPage1:
     .db escapeLargeFont, "RPN83P", Lenter
     .db escapeSmallFont, "v0.10.0-dev (2024", Shyphen, "02", Shyphen, "17)", Senter
-    .db "(c) 2023  Brian T. Park", Senter
+    .db "(c) 2023", Shyphen, "2024 Brian T. Park", Senter
     .db Senter
     .db "An RPN calculator for the", Senter
     .db "TI", Shyphen, "83 Plus and TI", Shyphen, "84 Plus", Senter
