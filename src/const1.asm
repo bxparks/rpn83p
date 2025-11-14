bxparks/rpn83p

commit abf289f29c174a243c2d08201bb795c0f457944f

universal.asm: move to Flash Page 1, as required by varsreg.asm

src/const1.asm
@@ -11,6 +11,18 @@ op1Set0PageOne:
     ld hl, const0PageOne
     jp move9ToOp1PageOne
 
+; Description: Set OP1 to 0.0. Faster version of bcall(_OP1Set0).
+; Destroys: all, HL
+op2Set0PageOne:
+    ld hl, const0PageOne
+    jp move9ToOp2PageOne
+
+; Description: Set OP1 to 0.0. Faster version of bcall(_OP1Set0).
+; Destroys: all, HL
+op4Set0PageOne:
+    ld hl, const0
+    jp move9ToOp4PageOne
+
 ;-----------------------------------------------------------------------------
 
 ; Description: Set OP2 to 100. The TI-OS Provides OP2Set60() but not

src/memory1.asm
@@ -140,6 +140,18 @@ move9ToOp3PageOne:
     ldir
     ret
 
+; Description: Move 9 bytes (size of TI-OS floating point number) from HL to
+; OP4. Implements bcall(_Mov9ToOP3) without the overhead of a bcall().
+; Input: HL=pointer to float
+; Output: (OP2)=contains float
+; Destroys: BC, DE, HL
+; Preserves: A
+move9ToOp4PageOne:
+    ld de, OP3
+    ld bc, 9
+    ldir
+    ret
+
 ; Description: Move 9 bytes (size of TI-OS floating point number) from OP1 to
 ; DE. Implements bcall(_MovFrOP1) without the overhead of a bcall().
 ; Input:
@@ -230,6 +242,17 @@ move22ToOp3PageOne:
     ldir
     ret
 
+; Input: HL=pointer to 2 floats in OPx registers.
+; Destroys: BC, DE, HL
+; Preserves: A
+move22ToOp5PageOne:
+    ld de, OP5
+    ld bc, 22
+    ldir
+    ret
+
+;-----------------------------------------------------------------------------
+
 ; Description: Move complex number in OP1/OP2 to OP3/OP4.
 ; Preserves: A
 cp1ToCp3PageOne:
@@ -240,6 +263,18 @@ cp1ToCp3PageOne:
 ; Preserves: A
 cp3ToCp1PageOne:
     ld hl, OP3
+    jr move22ToOp1PageOne
+
+; Description: Move complex number in OP1/OP2 to OP5/OP6.
+; Preserves: A
+cp1ToCp5PageOne:
+    ld hl, OP1
+    jr move22ToOp5PageOne
+
+; Description: Move complex number in OP5/OP6 to OP1/OP2.
+; Preserves: A
+cp5ToCp1PageOne:
+    ld hl, OP5
     jr move22ToOp1PageOne
 
 ; Description: Exchange OP1 with OP2. Inlined version of bcall(_OP1ExOP2) to

src/complex.asm
@@ -2,7 +2,7 @@
 ; MIT License
 ; Copyright (c) 2023 Brian T. Park
 ;
-; Routines related to complex numbers.
+; Routines related to complex numbers. TODO: No longer used, can be removed.
 ;-----------------------------------------------------------------------------
 
 ;-----------------------------------------------------------------------------
@@ -52,48 +52,3 @@ convertOp3ToCp3:
 ; Description: Throw an Err:DataType exception.
 convertErr:
     bcall(_ErrDataType)
-
-;-----------------------------------------------------------------------------
-; Complex result modes.
-;-----------------------------------------------------------------------------
-
-; Description: Update the TI-OS 'fmtFlags' from the 'numResult' variable in
-; RPN83P.
-updateNumResultMode:
-    ld a, (numResultMode)
-    cp numResultModeComplex
-    jr z, updateNumResultModeComplex
-    set fmtReal, (iy + fmtFlags)
-    res fmtRect, (iy + fmtFlags)
-    res fmtPolar, (iy + fmtFlags)
-    ret
-updateNumResultModeComplex:
-    res fmtReal, (iy + fmtFlags)
-    set fmtRect, (iy + fmtFlags)
-    res fmtPolar, (iy + fmtFlags)
-    ret
-
-; Description: Check if numResultMode is complex.
-; Output: ZF=1 if numResultMode==numResultModeComplex
-checkNumResultModeComplex:
-    ld a, (numResultMode)
-    cp numResultModeComplex
-    ret
-
-;-----------------------------------------------------------------------------
-; Complex display modes.
-;-----------------------------------------------------------------------------
-
-; Description: Sanitize the current complex display mode.
-updateComplexMode:
-    ld a, (complexMode)
-    cp a, complexModeRect
-    ret z
-    cp a, complexModeRad
-    ret z
-    cp a, complexModeDeg
-    ret z
-    ; Should not happen, but default RECT mode if it does.
-    ld a, complexModeRect
-    ld (complexMode), a
-    ret

src/complex1.asm
@@ -19,6 +19,53 @@ ColdInitComplex:
     ld (complexMode), a
     ret
 
+;-----------------------------------------------------------------------------
+; Complex result modes.
+;-----------------------------------------------------------------------------
+
+; Description: Update the TI-OS 'fmtFlags' from the 'numResult' variable in
+; RPN83P.
+UpdateNumResultMode:
+    ld a, (numResultMode)
+    cp numResultModeComplex
+    jr z, updateNumResultModeComplex
+    set fmtReal, (iy + fmtFlags)
+    res fmtRect, (iy + fmtFlags)
+    res fmtPolar, (iy + fmtFlags)
+    ret
+updateNumResultModeComplex:
+    res fmtReal, (iy + fmtFlags)
+    set fmtRect, (iy + fmtFlags)
+    res fmtPolar, (iy + fmtFlags)
+    ret
+
+; Description: Check if numResultMode is complex.
+; Output: ZF=1 if numResultMode==numResultModeComplex
+checkNumResultModeComplex:
+    ld a, (numResultMode)
+    cp numResultModeComplex
+    ret
+
+;-----------------------------------------------------------------------------
+; Complex display modes.
+;-----------------------------------------------------------------------------
+
+; Description: Sanitize the current complex display mode.
+UpdateComplexMode:
+    ld a, (complexMode)
+    cp a, complexModeRect
+    ret z
+    cp a, complexModeRad
+    ret z
+    cp a, complexModeDeg
+    ret z
+    ; Should not happen, but default RECT mode if it does.
+    ld a, complexModeRect
+    ld (complexMode), a
+    ret
+
+;------------------------------------------------------------------------------
+; Polar to Rect conversions.
 ;------------------------------------------------------------------------------
 
 ; Description: Convert OP1/OP2 (re,im) into complex number in CP1.
@@ -292,4 +339,52 @@ ComplexAngle:
     ret
 
 complexDataTypeErr:
+    bcall(_ErrDataType)
+
+;-----------------------------------------------------------------------------
+; Conversions from real numbers in OP1/OP2 to complex numbers in CP1.
+;-----------------------------------------------------------------------------
+
+; Description: Convert a real number in OP1 to a complex number in OP1/OP2 by
+; setting the OP2 to 0, and setting the objectType to complex. If already a
+; complex number, do nothing.
+; Input: OP1
+; Output: OP1/OP2
+; Destroys: A
+convertOp1ToCp1PageOne:
+    call checkOp1ComplexPageOne ; ZF=1 if complex
+    ret z
+    call checkOp1RealPageOne
+    jr nz, convertErrPageOne
+    call op2Set0PageOne
+    bcall(_RectToComplex)
+    ret
+
+; Description: Convert a complex number to real if the imaginary part is zero.
+; Do nothing if already a real.
+convertCp1ToOp1PageOne:
+    call checkOp1ComplexPageOne ; ZF-1 if complex
+    ret nz
+    bcall(_CkOP2FP0) ; ZF=1 if im(CP1)==0
+    ret nz
+    bcall(_ComplexToRect)
+    ret
+
+; Description: Convert a real number in OP3 to a complex number in OP3/OP4 by
+; setting the OP4 to 0, and setting the objectType to complex. If already a
+; complex number, do nothing.
+; Input: OP3
+; Output: OP3/OP4
+; Destroys: A
+convertOp3ToCp3PageOne:
+    call checkOp3ComplexPageOne ; ZF=1 if complex
+    ret z
+    call checkOp3RealPageOne
+    jr nz, convertErrPageOne
+    call op4Set0PageOne
+    bcall(_Rect3ToComplex3)
+    ret
+
+; Description: Throw an Err:DataType exception.
+convertErrPageOne:
     bcall(_ErrDataType)

src/complexmenuhandlers.asm
@@ -47,7 +47,7 @@ mNumResultModeRealHandler:
     call closeInputAndRecallNone
     ld a, numResultModeReal
     ld (numResultMode), a
-    call updateNumResultMode
+    bcall(_UpdateNumResultMode)
     set dirtyFlagsMenu, (iy + dirtyFlags)
     ret
 
@@ -67,7 +67,7 @@ mNumResultModeComplexHandler:
     call closeInputAndRecallNone
     ld a, numResultModeComplex
     ld (numResultMode), a
-    call updateNumResultMode
+    bcall(_UpdateNumResultMode)
     set dirtyFlagsMenu, (iy + dirtyFlags)
     ret
 

src/handlers.asm
@@ -661,7 +661,7 @@ handleKeyChs:
 handleKeyChsX:
     ; Not in edit mode, so change sign of X register
     bcall(_RclStackX)
-    call universalChs
+    bcall(_UniversalChs)
     bcall(_StoStackX)
     ret
 handleKeyChsInputBuf:
@@ -881,7 +881,7 @@ handleKeyAdd:
     bit rpnFlagsBaseModeEnabled, (iy + rpnFlags)
     jp nz, mBaseAddHandler
     call closeInputAndRecallUniversalXY ; CP1=Y; CP3=X
-    call universalAdd
+    bcall(_UniversalAdd)
     bcall(_ReplaceStackXY)
     ret
 
@@ -893,7 +893,7 @@ handleKeySub:
     bit rpnFlagsBaseModeEnabled, (iy + rpnFlags)
     jp nz, mBaseSubtHandler
     call closeInputAndRecallUniversalXY ; CP1=X; CP3=Y
-    call universalSub
+    bcall(_UniversalSub)
     bcall(_ReplaceStackXY)
     ret
 
@@ -905,7 +905,7 @@ handleKeyMul:
     bit rpnFlagsBaseModeEnabled, (iy + rpnFlags)
     jp nz, mBaseMultHandler
     call closeInputAndRecallUniversalXY ; CP1=Y; CP3=X
-    call universalMult
+    bcall(_UniversalMult)
     bcall(_ReplaceStackXY)
     ret
 
@@ -917,7 +917,7 @@ handleKeyDiv:
     bit rpnFlagsBaseModeEnabled, (iy + rpnFlags)
     jp nz, mBaseDivHandler
     call closeInputAndRecallUniversalXY ; CP1=Y; CP3=X
-    call universalDiv
+    bcall(_UniversalDiv)
     bcall(_ReplaceStackXY)
     ret
 
@@ -948,7 +948,7 @@ handleKeyEuler:
 ; Description: y^x
 handleKeyExpon:
     call closeInputAndRecallUniversalXY ; CP1=Y; CP3=X
-    call universalPow
+    bcall(_UniversalPow)
     bcall(_ReplaceStackXY)
     ret
 
@@ -962,7 +962,7 @@ handleKeyInv:
     jp z, mGenericDateCutHandlerAltEntry
     cp rpnObjectTypeOffsetDateTime
     jp z, mGenericDateCutHandlerAltEntry
-    call universalRecip
+    bcall(_UniversalRecip)
     bcall(_ReplaceStackX)
     ret
 
@@ -975,7 +975,7 @@ handleKeySquare:
     jp z, mGenericDateExtendHandlerAltEntry
     cp rpnObjectTypeDateTime
     jp z, mGenericDateExtendHandlerAltEntry
-    call universalSquare
+    bcall(_UniversalSquare)
     bcall(_ReplaceStackX)
     ret
 
@@ -988,7 +988,7 @@ handleKeySqrt:
     jp z, mGenericDateShrinkHandlerAltEntry
     cp rpnObjectTypeOffsetDateTime
     jp z, mGenericDateShrinkHandlerAltEntry
-    call universalSqRoot
+    bcall(_UniversalSqRoot)
     bcall(_ReplaceStackX)
     ret
 
@@ -1023,25 +1023,25 @@ handleKeyAns:
 
 handleKeyLog:
     call closeInputAndRecallUniversalX
-    call universalLog ; A=numReturnValues
+    bcall(_UniversalLog) ; A=numReturnValues
     bcall(_ReplaceStackX)
     ret
 
 handleKeyALog:
     call closeInputAndRecallUniversalX
-    call universalTenPow
+    bcall(_UniversalTenPow)
     bcall(_ReplaceStackX)
     ret
 
 handleKeyLn:
     call closeInputAndRecallUniversalX
-    call universalLn
+    bcall(_UniversalLn)
     bcall(_ReplaceStackX)
     ret
 
 handleKeyExp:
     call closeInputAndRecallUniversalX
-    call universalExp
+    bcall(_UniversalExp)
     bcall(_ReplaceStackX)
     ret
 

src/main.asm
@@ -39,8 +39,8 @@ warmInit:
     bcall(_InitArgBuf) ; Start with command ArgScanner off.
     bcall(_InitDisplay)
     bcall(_InitTvmSolver)
-    call updateNumResultMode
-    call updateComplexMode
+    bcall(_UpdateNumResultMode)
+    bcall(_UpdateComplexMode)
     bcall(_InitStack)
     bcall(_InitRegs)
     bcall(_InitStatRegs)

src/menuhandlers.asm
@@ -49,21 +49,21 @@ mHelpHandler:
 ; Description: Calculate X^3.
 mCubeHandler:
     call closeInputAndRecallUniversalX
-    call universalCube
+    bcall(_UniversalCube)
     bcall(_ReplaceStackX)
     ret
 
 ; Description: Calculate the cubic root of X, X^(1/3).
 mCubeRootHandler:
     call closeInputAndRecallUniversalX
-    call universalCubeRoot
+    bcall(_UniversalCubeRoot)
     bcall(_ReplaceStackX)
     ret
 
 ; Description: Calculate the X root of Y, Y^(1/X).
 mXRootYHandler:
     call closeInputAndRecallUniversalXY ; CP1=Y; CP3=X
-    call universalXRootY
+    bcall(_UniversalXRootY)
     bcall(_ReplaceStackXY)
     ret
 
@@ -95,21 +95,21 @@ mAtan2Handler:
 ; Description: TwoPow(X) = 2^X
 mTwoPowHandler:
     call closeInputAndRecallUniversalX
-    call universalTwoPow
+    bcall(_UniversalTwoPow)
     bcall(_ReplaceStackX)
     ret
 
 ; Description: Log2(X)=log(X)/log(2)
 mLog2Handler:
     call closeInputAndRecallUniversalX
-    call universalLog2
+    bcall(_UniversalLog2)
     bcall(_ReplaceStackX)
     ret
 
 ; Description: LogBase(Y,X)=log(Y)/log(X)
 mLogBaseHandler:
     call closeInputAndRecallUniversalXY ; CP1=Y; CP3=X
-    call universalLogBase
+    bcall(_UniversalLogBase)
     bcall(_ReplaceStackXY)
     ret
 

commit 11281e735ff1db33f334227ae2e7fd58d500c4ac

memory1.asm: Fix regression in Y^X and XROOTY functions in CRES mode when X is a Real.
Caused by abf289f29 which moved universal.asm to Flash Page 1. It had 2 bugs:

1) memory1.asm: Transcription error when copying move9ToOp4() from memory.asm to
move9ToOp4PageOne(). Target address was incorrectly given as to OP3 instead of
OP4. This broke op4Set0PageOne(), which broke convertOp3ToCp3PageOne(). This in
turn broke the conversion of real(X) to complex(X) in Y^X
(universalPowComplexToReal()), XROOTY (universalXRootYComplexByReal()), and 2^X
(universalTwoPowComplex()) because X is stored in OP4 in all three cases.

2) const1.asm: Forgot to rename 'const0' to 'const0PageOne' which caused the
imaginary part of the 'X' value to be set to 9 random bytes when it was
converted from a real a complex number.

src/const1.asm
@@ -20,7 +20,7 @@ op2Set0PageOne:
 ; Description: Set OP1 to 0.0. Faster version of bcall(_OP1Set0).
 ; Destroys: all, HL
 op4Set0PageOne:
-    ld hl, const0
+    ld hl, const0PageOne
     jp move9ToOp4PageOne
 
 ;-----------------------------------------------------------------------------

src/memory1.asm
@@ -108,7 +108,7 @@ move9ToOp3PageOne:
 ; Destroys: BC, DE, HL
 ; Preserves: A
 move9ToOp4PageOne:
-    ld de, OP3
+    ld de, OP4
     ld bc, 9
     ldir
     ret