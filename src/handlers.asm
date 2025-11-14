bxparks/rpn83p

commit 4a584f8136b036ebea21f3e612f4cc68460b214d

handlers.asm: call closeInputAndRecallNone() only if STO/RCL are NOT canceled (by ON/EXIT)

src/handlers.asm
@@ -1090,13 +1090,14 @@ handleKeyATan:
 ;-----------------------------------------------------------------------------
 
 handleKeySto:
-    call closeInputAndRecallNone
     ld hl, msgStoPrompt
     call startArgScanner
     set inputBufFlagsArgAllowModifier, (iy + inputBufFlags)
     set inputBufFlagsArgAllowLetter, (iy + inputBufFlags)
     call processArgCommands ; ZF=0 if cancelled
     ret nz ; do nothing if cancelled
+    ; close inputBuf only if STO is actually requested
+    call closeInputAndRecallNone
     cp argModifierIndirect
     ret nc ; TODO: implement this
     bcall(_RclStackX) ; OP1/OP2=X
@@ -1117,13 +1118,14 @@ handleKeyStoInvalid:
 ; 2) If the {op} is not empty, it is an arithmetic operator, we call
 ; RclOpGeneric() and *replace* the current X with the new X.
 handleKeyRcl:
-    call closeInputAndRecallNone
     ld hl, msgRclPrompt
     call startArgScanner
     set inputBufFlagsArgAllowModifier, (iy + inputBufFlags)
     set inputBufFlagsArgAllowLetter, (iy + inputBufFlags)
     call processArgCommands ; ZF=0 if cancelled
     ret nz ; do nothing if cancelled
+    ; close inputBuf only if RCL is actually requested
+    call closeInputAndRecallNone
     cp argModifierIndirect
     ret nc ; TODO: implement this
     ld a, (argValue) ; A=varLetter

commit bc2cf3c698ee106b17b9ca6e20f298eaa242bec6

handlers.asm: Fix STO regression, cannot store complex numbers and record objects.
Caused by 4a584f813 which implemented the feature to avoid termination of input
after an EXIT from a command argument prompt (e.g. STO, FIX, etc).
Unfortunately, moving the 'call closeInputAndRecallNone' to a later point in
the code caused the loss of A register to be lost, which identified the command
argument, which triggered an early return from the handler for complex and
record objects.

src/handlers.asm
@@ -1098,10 +1098,12 @@ handleKeySto:
     ret nz ; do nothing if cancelled
     ; close inputBuf only if STO is actually requested
     call closeInputAndRecallNone
+    ; Check for 'STO IND' which is unsupported right now.
+    ld a, (argModifier)
     cp argModifierIndirect
     ret nc ; TODO: implement this
     bcall(_RclStackX) ; OP1/OP2=X
-    ; Set up Sto parameters
+    ; Set up StoOpGeneric parameters
     ld a, (argValue) ; A=indexOrLetter
     ld c, a ; C=index or letter
     ld a, (argModifier)
@@ -1129,6 +1131,7 @@ handleKeyRcl:
     ret nz ; do nothing if cancelled
     ; close inputBuf only if RCL is actually requested
     call closeInputAndRecallNone
+    ld a, (argModifier)
     cp argModifierIndirect
     ret nc ; TODO: implement this
     ld a, (argValue) ; A=varLetter